98f7548e61a5b7b1a9255ab2f21effc60a1b41b4
alvin2723 <55695349+alvin2723@users.noreply.github.com>
2025-08-02 15:20:19 +0700

    Update streamInfo.extractor.js

--- src/extractors/streamInfo.extractor.js
+++ src/extractors/streamInfo.extractor.js
@@ -58,7 +58,7 @@ async function extractStreamingInfo(id, name, type) {
       name,
       type
     );
-    return { streamingLink, servers, isError: streamingLink === null ? true : false };
+    return { streamingLink, servers, isMegaCloudError: true };
   } catch (error) {
     console.error("An error occurred:", error);
     return { streamingLink: [], servers: [] };
